3ab07e216ac7262080b6173cefe32bba378e7fcc
GoGoGo2019 <xiangrui@aiursoft.com>
2020-07-13 07:15:13 +0000

    add common/email sender

--- app/common/email_sender_service/reply/send_email_reply.go
+++ app/common/email_sender_service/reply/send_email_reply.go
@@ -3,7 +3,7 @@ package reply
 import "go-common/library/ecode"
 
 type SendEmailReply struct {
-	ErrorCode ecode.Code
-	Message   string
-	Details   string
+	ErrorCode ecode.Code `json:"err_code"`
+	Message   string     `json:"message"`
+	Details   string     `json:"details"`
 }
